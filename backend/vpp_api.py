b8c813c777b65c1098562b5022aa53fa40b05513
zzangis34 <43721201+zzangis34@users.noreply.github.com>
2025-07-28 09:56:30 +0900

    asd

--- backend/vpp_api.py
+++ backend/vpp_api.py
@@ -150,4 +150,4 @@ def get_profit_rusult():
             "fail_reason": "server_error" 
             })
     
-app.run(debug=True, host='0.0.0.0', port=5000)
+app.run(debug=True, host='0.0.0.0', port=5001)
